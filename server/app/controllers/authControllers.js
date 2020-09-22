aeeb7b95ac5836fa9b894b2f0b746c9fe6b91060
Tacnik <roshan@tacnik.com>
2020-09-22 19:21:34 +0530

    register

--- server/app/controllers/authControllers.js
+++ server/app/controllers/authControllers.js
@@ -3,6 +3,22 @@ const User = require('../models/user')
 
 authControllers.register = (req,res) => {
     const { body } = req
+    User.findOne({email: body.email})
+        .then(user => {
+            if(user){
+                return res.status(422).json({
+                    ok: false,
+                    msg: 'Email already in use'
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                ok: false,
+                msg: `Can't process request. Try again later`,
+                err
+            })
+        })
     const user = new User(body)
         user.save()
         .then(user => {
