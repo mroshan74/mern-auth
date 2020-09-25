b61d967b44dd8fe3f3c9f8474f7bc38642361fae
Tacnik <roshan@tacnik.com>
2020-09-25 18:58:03 +0530

    server deploy step 1

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,16 @@ import Layout from './core/Layout'
 
 const App = () => (
   <Layout>
-    <h1>Page Handler</h1>
+      <div className="col-md-6 offset-md-3 text-center">
+          <h1 className="p-5">React Node MongoDB Authentication Boilerplate</h1>
+          <h2>MERN STACK</h2>
+          <hr />
+          <p className="lead">
+              MERN stack login register system with account activation, forgot password, reset password, login with facebook and google as well as private and protected routes for authenticated user and users with the role of admin.
+          </p>
+          <h4 className="p2">Others</h4>
+          <p className="lead">Profile update & deployment to digital ocean cloud servers</p>
+      </div>
   </Layout>
 )
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 5990
 const morgan = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
+const compression = require('compression')
+const path = require('path')
 
 if(process.env.NODE_ENV == 'development'){
     app.use(cors({origin: process.env.CLIENT_URL}))
@@ -17,6 +19,14 @@ db()
 
 const routes = require('./config/routes')
 app.use('/api',routes)
+
+//deploy
+app.use(compression())
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(port, () => {
     console.log('Listening on PORT -->',port)
